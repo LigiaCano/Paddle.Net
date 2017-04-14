b4e8d603360b2999bbf3ecf6fbb19a261784b0d5
LigiaCano <ligiacanorengifo@gmail.com>
2017-04-14 14:04:01 -0500

    Modelo Entity Paddle

---
+++ Paddle/Models/ModelPaddle.cs
@@ -0,0 +1,20 @@
+namespace Paddle.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class ModelPaddle : DbContext
+    {
+        public ModelPaddle()
+            : base("name=ModelPaddle")
+        {
+        }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
